d38e5dfb9867c355c5896710b09562fb2233d794
Rishab Chaudhary <rishab2245@gmail.com>
2024-10-05 14:38:28 +0530

    second

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 app.get("/" , (req,res)=>{
     res.json({message:"OK"})
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server is running")
 })
